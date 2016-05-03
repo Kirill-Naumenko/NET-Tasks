6869eb0458ca0b2c85bd0df601ce9f7412925179
kirill.naumenko <Kiryl_Navumenka@epam.com>
2016-05-03 10:26:48 +0300

    Part 7 exceptions

---
+++ Part 7 exp/Exceptions/Button.cs
@@ -0,0 +1,55 @@
+﻿using Exceptions.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Button : BaseElement, IClickable
+    {
+
+        public string name;
+        public string state;
+
+
+        public Button(string name, string state)
+        {
+            this.name = name;
+            this.state = state;
+        }
+
+        public Button()
+        {
+            this.name = null;
+            this.state = null;
+        }
+
+        public Button(string name)
+        {
+            this.name = name;
+        
+        }
+
+
+        public string Click()
+        {
+            if (this.state.Equals("enable"))
+            {
+                return this.name +" is enable";
+            }
+            if (this.state.Equals("disable"))
+            {
+                throw new ButtonIsDisabledException(this.name+" button is disabled");
+            }
+            if(this.state.Equals(""))
+            {
+                throw new ButtonNotExistException (this.name+" not exist");
+            }
+
+            throw new  NotValidData(this.name + " not valid data at button");
+
+        }
+    }
+}
---
+++ Part 7 exp/Exceptions/Page.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Exceptions.Exceptions;
+
+namespace Exceptions
+{
+    class Page
+    {
+
+        public List<Button> buttons;
+
+        
+
+        public Page()
+        {
+            buttons = new List<Button>();
+        }
+
+        public void GetButtons(string path)
+        {
+            XDocument doc = XDocument.Load(path);
+            var temp = doc.Element("elements").Element("buttons").Elements("button").ToList();
+
+
+
+
+            foreach (var x in temp)
+            {
+                
+                
+                    this.buttons.Add(new Button(x.Element("name").Value));
+
+            }
+            for (int i = 0; i < this.buttons.Count; i++)
+            {
+                for (int j = i+1; j < this.buttons.Count-1; j++)
+                {
+                    if (buttons.ElementAt(i).name.Equals(buttons.ElementAt(j).name))
+                    {
+                        throw new NotValidData("");
+                    }
+                
+                }
+            }
+
+            
+
+           
+           
+
+           
+        }
+
+
+        public void  checkButtons(string path)
+        {
+            XDocument doc = XDocument.Load(path);
+            var state = (from b in doc.Element("buttons").Elements("button")
+                         from name in b.Elements("name")
+                         from stat in b.Elements("state")
+                         select new
+                         {
+                             name = (string)name,
+                             state = (string)stat
+                         }).ToDictionary(temp => temp.name.ToString(), temp => temp.state.ToString());
+
+            for (int i = 0; i < this.buttons.Count; i++)
+            {
+                if (state.ContainsKey(this.buttons.ElementAt(i).name)){
+                    buttons.ElementAt(i).state = state[this.buttons.ElementAt(i).name];
+                }
+                else
+                {
+                    buttons.ElementAt(i).state = "";
+                }
+            }
+
+
+            
+
+            
+
+        }
+
+
+        
+
+    }
+}
